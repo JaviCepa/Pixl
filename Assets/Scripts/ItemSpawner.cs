22247d0bb5783d8dcbf16a88eaa17ba8f0db5a69
Javi Cepa <javicepa@gmail.com>
2017-12-15 20:33:11 +0100

    Project cleanup

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoinSpawner : MonoBehaviour {
+public class ItemSpawner : MonoBehaviour {
 
 	public GameObject coinPrefab;
 	public GameObject obstaclePrefab;
